e8fa9b124c253a237d2f1d6b3e406f4ab4cd7c8c
noashapiro <noashapiro@gmail.com>
2021-12-28 20:27:15 +0200

    added Assignments 4

---
+++ Assignment 4/src/WhitePony.java
@@ -0,0 +1,10 @@
+public class WhitePony extends Pony{
+
+    public WhitePony(String name, int age) {
+        super(name, age);
+    }
+
+    public String getPonyColor(){
+        return "white";
+    }
+}
