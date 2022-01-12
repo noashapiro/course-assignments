eaceafbf487509942851bc37916b5ae751e48a80
noashapiro <noashapiro@gmail.com>
2022-01-12 23:02:20 +0200

    update

--- Assignments 8/Assignments8/src/main/java/Main.java
+++ Assignments 8/Assignments8/src/main/java/Main.java
@@ -3,37 +3,93 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class Main {
     private static WebDriver chromeDriver;
     private static WebDriver firFoxDriver;
 
-
-    public static void main(String[] args) {
+    @BeforeClass
+    public static void beforeAll(){
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\97250\\ChromeDriver96\\chromedriver.exe");
-        chromeDriver = new ChromeDriver();
-
         System.setProperty("webdriver.gecko.driver","C:\\Users\\97250\\FireFoxDriver\\geckodriver.exe");
+        chromeDriver = new ChromeDriver();
         firFoxDriver = new FirefoxDriver();
-
-        answerThree();
-        answerFour();
     }
 
-    public static void answerThree(){
+    //1
+    @Test
+    public static void open_walla_chrome(){
+        chromeDriver.get("https://www.walla.co.il");
+    }
+
+    //1
+    @Test
+    public static void ynet_firefox(){
+        firFoxDriver.get("https://www.ynet.co.il/");
+    }
+
+    //2
+    @Test
+    public static void googleTranslate(){
         chromeDriver.get("https://translate.google.com/");
         WebElement element = chromeDriver.findElement(By.className("er8xn"));
         WebElement element1 = chromeDriver.findElement(By.cssSelector("textarea[aria-controls=kvLWu]"));
         System.out.println(element);
         System.out.println(element1);
-        chromeDriver.quit();
     }
 
-    public static void answerFour(){
+    //3
+    @Test
+    public static void youtube(){
         firFoxDriver.get("https://www.youtube.com/");
         WebElement element = firFoxDriver.findElement(By.id("logo-icon"));
         System.out.println(element);
-        firFoxDriver.quit();
+    }
 
+    //4
+    @Test
+    public static void selenium(){
+        firFoxDriver.get("https://www.selenium.dev/");
+        WebElement element = firFoxDriver.findElement(By.className("form-control"));
+        System.out.println(element);
+    }
+
+    //5
+    @Test
+    public static void amazon_test(){
+        chromeDriver.get("https://www.amazon.com/");
+        String amazonTitle = "Amazon.com. Spend less. Smile more.";
+        Assert.assertEquals(chromeDriver.getTitle(), amazonTitle);
+        chromeDriver.findElement(By.id("twotabsearchtextbox")).sendKeys("Leather shoes");
+    }
+
+    //6
+    @Test
+    public static void translate_search(){
+        chromeDriver.get("https://translate.google.com/");
+        chromeDriver.findElement(By.className("er8xn")).sendKeys("שלום");
+    }
+
+    //7
+    @Test
+    public static void youtube_search_song(){
+        chromeDriver.get("https://www.youtube.com/");
+        chromeDriver.manage().window().maximize();
+        WebElement search = chromeDriver.findElement(By.id("search"));
+        search.click();
+        search.sendKeys("fix you");
+
+    }
+
+
+
+    @AfterClass
+    public static void afterAll(){
+      /*  chromeDriver.quit();
+        firFoxDriver.quit();*/
     }
 }
