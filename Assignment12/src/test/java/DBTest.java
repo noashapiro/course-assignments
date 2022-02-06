a3d46ad9f0aa344ad4002831e73557ff24ba87cd
noashapiro <noashapiro@gmail.com>
2022-02-06 21:40:39 +0200

    added answer 1

--- Assignment12/src/test/java/DBTest.java
+++ Assignment12/src/test/java/DBTest.java
@@ -1,7 +1,30 @@
-public class DBTest {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DBTest {
+    private static final String USER_NAME = "XFjA4XJrNw";
+    private static final String DATABASE_NAME = "XFjA4XJrNw";
+    private static final String PASSWORD = "1MyvHdYTQv";
+    private static final String PORT = "3306";
+    private static final String SERVER = "remotemysql.com";
+
+    public static void main(String[] args) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:mysql://"+SERVER+":"+PORT, USER_NAME, PASSWORD);
+        //createTable(con);
+        insertDogs(con, "Bel", 2,"King charles");
+        insertDogs(con, "Niki", 10,"Border kolie");
+        insertDogs(con, "Mika", 6,"Labrador");
     }
 
+    private static void createTable(Connection con) throws SQLException {
+        String statementToExecute = "CREATE TABLE " + DATABASE_NAME + ".`dogs`(`name` VARCHAR(40) NOT NULL, `age` INT NOT NULL,`breed` VARCHAR(30) NOT NULL);";
+        con.createStatement().execute(statementToExecute);
+    }
+
+    private static void insertDogs(Connection con, String name, int age, String breed) throws SQLException {
+        String statementToExecute = "INSERT INTO " + DATABASE_NAME + ".dogs (`name`, `age`,`breed`) VALUES ('" + name + "', '" + age + "', '" + breed + "');";
+        con.createStatement().execute(statementToExecute);
+    }
 
 }
