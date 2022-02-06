f68456748d10cc20a0385403ecacaf888ef32860
noashapiro <noashapiro@gmail.com>
2022-02-06 22:07:19 +0200

    added answer 1-5

--- Assignment12/src/test/java/DBTest.java
+++ Assignment12/src/test/java/DBTest.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBTest {
     private static final String USER_NAME = "XFjA4XJrNw";
@@ -12,9 +10,18 @@ public class DBTest {
     public static void main(String[] args) throws SQLException {
         Connection con = DriverManager.getConnection("jdbc:mysql://"+SERVER+":"+PORT, USER_NAME, PASSWORD);
         //createTable(con);
-        insertDogs(con, "Bel", 2,"King charles");
-        insertDogs(con, "Niki", 10,"Border kolie");
-        insertDogs(con, "Mika", 6,"Labrador");
+
+        //insertDogs(con, "Bel", 2,"King charles");
+        //insertDogs(con, "Niki", 10,"Border kolie");
+        //insertDogs(con, "Mika", 6,"Labrador");
+
+        // updateDogAge(con,12,"Niki");
+
+        //deleteDogByName(con, "Mika");
+
+        getTableContent(con);
+
+        con.close();
     }
 
     private static void createTable(Connection con) throws SQLException {
@@ -25,6 +32,30 @@ public class DBTest {
     private static void insertDogs(Connection con, String name, int age, String breed) throws SQLException {
         String statementToExecute = "INSERT INTO " + DATABASE_NAME + ".dogs (`name`, `age`,`breed`) VALUES ('" + name + "', '" + age + "', '" + breed + "');";
         con.createStatement().execute(statementToExecute);
+    }
+
+    private static void updateDogAge(Connection con, int age, String name) throws SQLException {
+        String statementToExecute = "UPDATE `" + DATABASE_NAME + "`.`dogs` SET `age`='"+age+"' WHERE `name`='"+name+"';";
+        con.createStatement().executeUpdate(statementToExecute);
+    }
+
+    private static void deleteDogByName(Connection con, String name) throws SQLException {
+        String statementToExecute = "DELETE FROM `" + DATABASE_NAME + "`.`dogs` WHERE `name`='"+name+"';";
+        con.createStatement().execute(statementToExecute);
+    }
+
+    private static void getTableContent(Connection con) throws SQLException {
+        String statementToExecute = "SELECT * FROM " + DATABASE_NAME + ".dogs;";
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(statementToExecute);
+        while(rs.next()){
+            //Retrieve by column name
+            String name = rs.getString("name");
+
+            //Display values
+            System.out.print(" Name: " + name);
+        }
+        rs.close();
     }
 
 }
